4448e395d25b9fab9cc2c4198dae8d5416441a3d
Mateus Costa <mateusespindola25@hotmail.com>
2021-10-11 23:26:31 -0300

    changed port number

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 80;                  //Save the port number where your server will be listening
+const port = process.env.PORT || 80;              //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
